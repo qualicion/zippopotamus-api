e305dd0dfb3ed7e5c2152deb4615a487b44f472e
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-23 15:09:23 +0100

    updated jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  testMatch: ["**/specs/**/*.spec.ts"],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  reporters: ["default", ["jest-junit", { outputDirectory: "reports" }], ['jest-html-reporters', {publicPath: 'reports'}]]
 };
