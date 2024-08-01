c96ac2c95ee42fca8ce63c717f987f3a14ba0948
Babatunde Oduniyi <qualicion@outlook.com>
2024-08-01 07:57:24 +0100

    added data file

---
+++ data/codes.ts
@@ -0,0 +1,4 @@
+export default {
+    country: ['us' ],
+    code: ['90210', '00210']
+}
--- specs/zippotamus.spec.ts
+++ specs/zippotamus.spec.ts
@@ -1,11 +1,18 @@
 import controller from "../controller/zippotamus.controller";
+import data from "../data/codes";
+import config from "../config/config.baseurl";
+
 
 describe("Zippotamus API", () => {
+  data.country.forEach(country=> {
+    data.code.forEach(code => {
   it("GET address", async () => {
-    const response = await controller.getAddress("/us/90210");
+    const response = await controller.getAddress(`/${country}/${code}`);
     expect(response.status).toEqual(200);
   });
+})
 
+})
   it("should check country and postal code", async () => {
     const response = await controller.getAddress("/us/90210");
     expect(response.body.country).toEqual("United States");
@@ -13,11 +20,11 @@ describe("Zippotamus API", () => {
   });
 
   it("should return 404 with an invalid endpoint", async () => {
-    const response = await controller.getAddress("/u");
+    const response = await controller.getAddress(config.invalidUrl);
     expect(response.status).toEqual(404);
   });
 
-  it("should check country and postal code", async () => {
+  it("should check response header and content type", async () => {
     const response = await controller.getAddress("/us/90210");
     expect(response.headers["content-type"]).toContain("/json");
   });
