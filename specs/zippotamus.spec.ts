5247f970af4df572ef49476795f6d9f5c3d65d5c
Babatunde Oduniyi <qualicion@outlook.com>
2024-08-01 09:59:52 +0100

    updated test to reflect the changes across framework

--- specs/zippotamus.spec.ts
+++ specs/zippotamus.spec.ts
@@ -1,31 +1,37 @@
 import controller from "../controller/zippotamus.controller";
-import data from "../data/codes";
+import { getAddress } from "../utils/helper";
 import config from "../config/config.baseurl";
 
-
 describe("Zippotamus API", () => {
-  data.country.forEach(country=> {
-    data.code.forEach(code => {
-  it("GET address", async () => {
-    const response = await controller.getAddress(`/${country}/${code}`);
-    expect(response.status).toEqual(200);
-  });
-})
+  describe("Check status and structure", () => {
+    let responses;
 
-})
-  it("should check country and postal code", async () => {
-    const response = await controller.getAddress("/us/90210");
-    expect(response.body.country).toEqual("United States");
-    expect(response.body["post code"]).toEqual("90210");
+    beforeAll(async () => {
+      responses = await getAddress();
+    });
+
+    it("should check country and postal code", async () => {
+      responses.forEach((response) => {
+        expect(response.status).toEqual(200);
+      });
+    });
+
+    it("should check country and postal code", async () => {
+      const response = await controller.getAddress("/us/90210");
+      expect(response.body.country).toEqual("United States");
+      expect(response.body["post code"]).toEqual("90210");
+    });
   });
 
-  it("should return 404 with an invalid endpoint", async () => {
-    const response = await controller.getAddress(config.invalidUrl);
-    expect(response.status).toEqual(404);
-  });
+  describe("Negative and header tests", () => {
+    it("should return 404 with an invalid endpoint", async () => {
+      const response = await controller.getAddress(config.invalidUrl);
+      expect(response.status).toEqual(404);
+    });
 
-  it("should check response header and content type", async () => {
-    const response = await controller.getAddress("/us/90210");
-    expect(response.headers["content-type"]).toContain("/json");
+    it("should check response header and content type", async () => {
+      const response = await controller.getAddress("/us/90210");
+      expect(response.headers["content-type"]).toContain("/json");
+    });
   });
 });
